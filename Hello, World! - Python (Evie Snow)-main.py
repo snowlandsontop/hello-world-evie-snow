169f6c039041d1796572467749d0a9ffbb986a70
snowlandsontop <152302239+snowlandsontop@users.noreply.github.com>
2023-12-06 10:17:33 -0500

    Add files via upload

---
+++ Hello, World! - Python (Evie Snow)-main.py
@@ -0,0 +1,9 @@
+# Imports go at the top
+from microbit import *
+
+
+# Code in a 'while True:' loop repeats forever
+while True:
+    display.scroll('EVIE')
+    sleep(1000)
+    display.scroll('EVIE')
